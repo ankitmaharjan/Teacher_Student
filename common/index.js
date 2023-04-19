ae99b91a6478fc53cccb8a27154243373eef56fe
Ankit Maharjan <ankit.maharjan@swifttech.com>
2023-04-19 04:01:37 -0700

    initial commit

--- common/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(() => {
-    module.exports = {
-        mysqlHelper: require("./mysqlHelper"),
-        hasher: require("./../controller/helper/hasher"),
-        validateHelper: require("./../controller/helper/validationHelper")
-    }
-})()
--- controller/helper/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(() => {
-    module.exports = {
-        mysqlHelper: require("../../common/mysqlHelper"),
-        validateHelper: require("../helper/validationHelper"),
-        hasher: require("../helper/hasher")
-    }
-})()
